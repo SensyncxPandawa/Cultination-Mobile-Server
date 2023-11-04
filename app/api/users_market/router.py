b43727a1541ac45d427a7df6188b209a1f40d65e
Server - Yahya Aqrom <aqrom.yahya75@gmail.com>
2023-11-04 03:35:40 +0000

    Update Users' Market

--- app/api/users_market/router.py
+++ app/api/users_market/router.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
 from . import services
-from .schemas import UsersMarket, SetUsersMarket, UserProductionCapacity, SetUserProductionCapacity, UserMarketCapacity, SetUserMarketCapacity, UserMarketPreference, SetUserMarketPreference
+from .schemas import UsersMarket, SetUsersMarket
 from app.database import get_db
 
 router = APIRouter()
@@ -13,31 +13,7 @@ router = APIRouter()
 def display_existing_user_market(user_id: int, db: Session = Depends(get_db)):
     return services.display_existing_user_market(db, user_id)
 
-@router.get("/users/{user_id}/market/production_capacity", response_model=UserProductionCapacity, tags=["Users' Market"])
-def display_existing_user_production_capacity(user_id: int, db: Session = Depends(get_db)):
-    return services.display_existing_user_market(db, user_id)
-
-@router.get("/users/{user_id}/market/market_capacity", response_model=UserMarketCapacity, tags=["Users' Market"])
-def display_existing_user_market_capacity(user_id: int, db: Session = Depends(get_db)):
-    return services.display_existing_user_market(db, user_id)
-
-@router.get("/users/{user_id}/market/market_preference", response_model=UserMarketPreference, tags=["Users' Market"])
-def display_existing_user_market_preference(user_id: int, db: Session = Depends(get_db)):
-    return services.display_existing_user_market(db, user_id)
-
 # SET USER DATA
 @router.put("/users/{user_id}/market", response_model=UsersMarket, tags=["Users' Market"])
 def update_user_market_by_id(user_id: int, updated_user_market: SetUsersMarket, db: Session = Depends(get_db)):
-    return services.update_user_market_by_id(db, user_id, updated_user_market)
-
-@router.put("/users/{user_id}/market/production_capacity", response_model=UserProductionCapacity, tags=["Users' Market"])
-def update_user_production_capacity_by_id(user_id: int, updated_user_market: SetUserProductionCapacity, db: Session = Depends(get_db)):
-    return services.update_user_market_by_id(db, user_id, updated_user_market)
-
-@router.put("/users/{user_id}/market/market_capacity", response_model=UserMarketCapacity, tags=["Users' Market"])
-def update_user_market_capacity_by_id(user_id: int, updated_user_market: SetUserMarketCapacity, db: Session = Depends(get_db)):
-    return services.update_user_market_by_id(db, user_id, updated_user_market)
-
-@router.put("/users/{user_id}/market/market_preference", response_model=UserMarketPreference, tags=["Users' Market"])
-def update_user_market_preference_by_id(user_id: int, updated_user_market: SetUserMarketPreference, db: Session = Depends(get_db)):
     return services.update_user_market_by_id(db, user_id, updated_user_market)
--- app/api/users_market/services.py
+++ app/api/users_market/services.py
@@ -5,36 +5,25 @@ from .schemas import UsersMarket
 
 # DISPLAY USER DATA
 def display_existing_user_market(db: Session, user_id: int):
-    users_market = db.query(models.UsersMarket).filter_by(user_id=user_id).first()
-    if users_market is None:
+    user_market = db.query(models.UsersMarket).filter_by(user_id=user_id).first()
+    if user_market is None:
         raise HTTPException(status_code=404, detail="User ID not found")
 
-    return users_market
+    return user_market
 
 # SET USER DATA
 def update_user_market_by_id(db: Session, user_id: int, updated_users_market: UsersMarket):
-    users_market = db.query(models.UsersMarket).filter_by(user_id=user_id).first()
-    if users_market is None:
+    user_market = db.query(models.UsersMarket).filter_by(user_id=user_id).first()
+    if user_market is None:
         raise HTTPException(status_code=404, detail="User class not found")
 
-    # Update only the provided fields from updated_users_class
-    if hasattr(updated_users_market, 'user_production_capacity_n'):
-        users_market.user_production_capacity_n = updated_users_market.user_production_capacity_n
-    if hasattr(updated_users_market, 'user_production_capacity_unit'):
-        users_market.user_production_capacity_unit = updated_users_market.user_production_capacity_unit
-    if hasattr(updated_users_market, 'user_production_capacity_cycle'):
-        users_market.user_production_capacity_cycle = updated_users_market.user_production_capacity_cycle
-    if hasattr(updated_users_market, 'user_market_capacity_n'):
-        users_market.user_market_capacity_n = updated_users_market.user_market_capacity_n
-    if hasattr(updated_users_market, 'user_market_capacity_unit'):
-        users_market.user_market_capacity_unit = updated_users_market.user_market_capacity_unit
-    if hasattr(updated_users_market, 'user_market_capacity_cycle'):
-        users_market.user_market_capacity_cycle = updated_users_market.user_market_capacity_cycle
-    if hasattr(updated_users_market, 'user_market_preference'):
-        users_market.user_market_preference = updated_users_market.user_market_preference
+    # Update the user's data based on the provided input
+    for attr, value in updated_users_market.dict().items():
+        if attr != "user_id" and hasattr(user_market, attr) and value is not None:
+            setattr(user_market, attr, value)
 
     # Commit changes to the database
     db.commit()
 
-    return users_market
+    return user_market
 
